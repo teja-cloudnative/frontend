11529c04e5265fa3689f5d7804d7469475cf78c1
tejap <btejapramod@gmail.com>
2022-02-24 16:16:16 +0530

    New Changes

--- mutable/main.tf
+++ mutable/main.tf
@@ -1,7 +1,9 @@
 module "ec2"{
   source = "git:github.com/teja-cloudnative/terraform-mutable-ec2"
-  OD_INSTANCE_COUNT = var.OD_INSTANCE_COUNT
+  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
+  OD_INSTANCE_TYPE    = var.OD_INSTANCE_TYPE
+  SPOT_INSTANCE_TYPE  = var.SPOT_INSTANCE_TYPE
 }
 
 #module "alb" {
