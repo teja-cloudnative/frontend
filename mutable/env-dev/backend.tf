8e5b5884bee303f3839bcf7e2d6d0ac16c5f5c8b
tejap <btejapramod@gmail.com>
2022-02-26 16:33:57 +0530

    New Changes

--- mutable/env-dev/backend.tf
+++ mutable/env-dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-teja"
-    key    = "mutable/frontend/prod/terraform.tfstate"
+    key    = "mutable/frontend/dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
