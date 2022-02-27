439155edc0a11e11c6417094053a355628419533
tejap <btejapramod@gmail.com>
2022-02-27 20:09:33 +0530

    New Changes

--- mutable/env-dev/main.tf
+++ mutable/env-dev/main.tf
@@ -1,12 +1,18 @@
 module "ec2" {
-  source              = "git::github.com/teja-cloudnative/terraform-mutable-ec2"
-  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
+  source              = "git::https://github.com/raghudevopsb62/terraform-mutable-ec2"
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
-  OD_INSTANCE_TYPE    = var.OD_INSTANCE_TYPE
+  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
   SPOT_INSTANCE_TYPE  = var.SPOT_INSTANCE_TYPE
+  OD_INSTANCE_TYPE    = var.OD_INSTANCE_TYPE
   ENV                 = var.ENV
   COMPONENT           = var.COMPONENT
+  ALB_ATTACH_TO       = "frontend"
 }
-#module "alb" {
-#  source = "git::github.com/teja-cloudnative/terraform-mutable-alb"
-#}
+
+module "tags" {
+  count       = length(module.ec2.ALL_TAGS)
+  source      = "git::https://github.com/raghudevopsb62/terraform-tags"
+  TAG_NAME    = lookup(element(module.ec2.ALL_TAGS, count.index), "name")
+  TAG_VALUE   = lookup(element(module.ec2.ALL_TAGS, count.index), "value")
+  RESOURCE_ID = module.ec2.ALL_TAG_IDS
+}
